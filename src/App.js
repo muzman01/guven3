59775a60270c835ecad578451fff4dce4178d81d
muzman01 <supurge_man@hotmail.com>
2023-08-02 20:54:50 +0300

    guven3

--- src/App.js
+++ src/App.js
@@ -1,22 +1,32 @@
-import logo from './logo.svg';
+import React, { useEffect, useState } from "react";
 import './App.css';
+import guvenABI from "./ABI.json"
+
+import Web3 from "web3";
 
 function App() {
+  const web3 = new Web3("https://api.avax-test.network/ext/bc/C/rpc")
+  const contractAddress = "0xff39f96d6560F769D5a1f30fFd4aa044AF3296b5"
+  const [account, setAccount] = useState("")
+  async function requestConnect() {
+    const result = await window.ethereum.request({ method: "eth_requestAccounts" });
+    const account = (await window.ethereum.request({ method: "eth_accounts" }))[0]
+    setAccount(web3.utils.toChecksumAddress(account))
+  }
+
+
+  const handleTest = async () => {
+    const Contract = new web3.eth.Contract(guvenABI, contractAddress);
+
+    const result = await Contract.methods.newPayload("deneme").call()
+    console.log(result);
+
+  }
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <button onClick={requestConnect} style={{ cursor: "pointer" }}>{account ? account : "bağlan"}</button>
+        <button onClick={handleTest} style={{ cursor: "pointer" }}>deneme</button>
       </header>
     </div>
   );
